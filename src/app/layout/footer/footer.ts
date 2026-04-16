28fca0dfe3853facf7e0713b308aa0492a3005c1
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-04-16 16:45:31 +0530

    update footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Button } from 'primeng/button';
+import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, Button, Divider],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  currentYear = new Date().getFullYear();
+}
