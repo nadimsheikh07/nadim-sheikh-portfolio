aa655182f0e046352d9d03e254a88be3f54c5e74
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-04-18 22:30:56 +0530

    fix btn

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Button } from 'primeng/button';
 import { Divider } from 'primeng/divider';
 import { PROFILE } from '../../shared/data/profile';
 import { PROFILE_LINKS } from '../../shared/data/profile-links';
@@ -9,7 +8,7 @@ import { ProfileLink } from '../../shared/models/profile-link';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule, RouterLink, Button, Divider],
+  imports: [CommonModule, RouterLink, Divider],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
