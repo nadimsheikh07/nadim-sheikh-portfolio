89f3869e8f5907d4f2b2f872dac31bb3208a40a0
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-04-18 14:00:03 +0530

    design footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Button } from 'primeng/button';
 import { Divider } from 'primeng/divider';
+import { PROFILE } from '../../shared/data/profile';
+import { PROFILE_LINKS } from '../../shared/data/profile-links';
+import { ProfileLink } from '../../shared/models/profile-link';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, Button, Divider],
+  imports: [CommonModule, RouterLink, Button, Divider],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   currentYear = new Date().getFullYear();
+  readonly profile = PROFILE;
+  readonly profileLinks: ProfileLink[] = PROFILE_LINKS;
+  readonly quickLinks = [
+    { label: 'Home', route: '/home' },
+    { label: 'About', route: '/about' },
+    { label: 'Projects', route: '/projects' },
+    { label: 'Contact', route: '/contact' },
+  ];
 }
